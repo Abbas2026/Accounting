ad2ccc7f3644741b9da6b26f8b68f1e1a8ea46aa
abbas <avavav060@gmail.com>
2025-05-06 14:05:32 +0430

    Create a project

---
+++ sql/Products.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Products (
+    ProductID INT IDENTITY(1,1) PRIMARY KEY,
+    ProductCode VARCHAR(50) UNIQUE NOT NULL,
+    ProductName NVARCHAR(100) NOT NULL,
+    BuyPrice INT NOT NULL,
+    SellPrice INT NOT NULL,
+    Stock INT NOT NULL
+);
